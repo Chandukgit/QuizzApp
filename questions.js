a9fd436120fb3c681210eb1b160cb782d3e46739
Chandukgit <117837728+Chandukgit@users.noreply.github.com>
2022-11-26 10:53:41 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions=[
+{
+    numb: 1,
+    question:"what does the HTML stands for ?",
+    answer: "hyper text markup language",
+    options:[
+     "hypertext processo language",
+     "hyper text markup language",
+     "hyper texr transfer language",
+     "hyper tool multilanguage"
+    ]
+ 
+},
+{
+    numb: 2,
+    question:"examples of polymorphism?",
+    answer: "methodoverloading and method overriding",
+    options:[
+     "inheritance",
+     "abstraction",
+     "methodoverloading and method overriding",
+     "ducktyping"
+    ]
+ 
+},
+{
+    numb: 3,
+    question:"what does the oops stands for ?",
+    answer: "object oriented programming",
+    options:[
+     "object oriented programming",
+     "object occasion language",
+     "object ops programs",
+     "object orentation program"
+    ]
+ 
+},
+{
+    numb: 4,
+    question:"python is ____ language ?",
+    answer: "interpretered language",
+    options:[
+     "markup language",
+     "compiled and dynamic typed language",
+     "compiled language",
+     "interpretered language"
+    ]
+ 
+},
+{
+    numb: 5,
+    question:"bootstrap is a ?",
+    answer: "css frame work",
+    options:[
+     "java frame work",
+     "css frame work",
+     "python frame work",
+     "django frame work"
+    ]
+ 
+}  
+]
